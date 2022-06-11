a71cb445441e118e352243b2ad1d48677720e89e
Katy Ereira <katy@skillshare.com>
2022-06-11 11:55:12 +0100

    chore: extract functions

--- main.go
+++ main.go
@@ -9,15 +9,18 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/user/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-
-		fmt.Fprintf(w, "Hello, %s!", vars["name"])
-	})
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
-	})
+	r.HandleFunc("/user/{name}", HelloUser)
+	r.HandleFunc("/", HelloWorld)
 
 	http.ListenAndServe(":80", r)
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+func HelloUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "Hello, %s!", vars["name"])
 }
