d88bcb3735627b60ca7830a5a70e7d7166b12805
Katy Ereira <katy@skillshare.com>
2022-06-11 11:39:16 +0100

    feat: add gorilla/mux router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module maccath/go-webapp
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- index.go
+++ index.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/user/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		fmt.Fprintf(w, "Hello, %s!", vars["name"])
 	})
 
-	http.ListenAndServe(":80", nil)
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!")
+	})
+
+	http.ListenAndServe(":80", r)
 }
