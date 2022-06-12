e25b4018f65cd019760819fded54adac02411c15
Katy Ereira <katy@skillshare.com>
2022-06-12 17:03:04 +0100

    feat: only load .env if exists

--- cmd/main.go
+++ cmd/main.go
@@ -11,18 +11,30 @@ import (
 )
 
 func main() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	loadEnv()
 
 	var repo = memory.Repository{}
 	repo.Save(memory.User{Name: "katy"})
 
 	var g = greeting.NewService(repo)
 
-	port := os.Getenv("PORT")
+	http.ListenAndServe(":"+getPort(), web.Handler(g))
+}
 
-	http.ListenAndServe(":"+port, web.Handler(g))
+func getPort() string {
+	port, specified := os.LookupEnv("PORT")
+	if !specified {
+		port = "80"
+	}
+	return port
+}
+
+func loadEnv() {
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load(".env")
+
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
 }
