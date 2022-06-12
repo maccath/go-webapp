cc8a6015aef963fa9a411187739eba2bb8c5133c
Katy Ereira <katy@skillshare.com>
2022-06-12 16:52:30 +0100

    feat: add dotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/maccath/go-webapp/pkg/greeting"
 	"github.com/maccath/go-webapp/pkg/http/web"
 	"github.com/maccath/go-webapp/pkg/storage/memory"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	var repo = memory.Repository{}
 	repo.Save(memory.User{Name: "katy"})
 
 	var g = greeting.NewService(repo)
 
 	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "80"
-	}
 
 	http.ListenAndServe(":"+port, web.Handler(g))
 }
